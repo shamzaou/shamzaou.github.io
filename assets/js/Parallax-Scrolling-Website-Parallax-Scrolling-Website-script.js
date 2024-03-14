09944ef5ded1425d79115d7b3876c1eff7a00d0d
shamzaou <shamzaou@student.42abudhabi.ae>
2024-03-14 15:24:30 +0400

    Website updates.

---
+++ assets/js/Parallax-Scrolling-Website-Parallax-Scrolling-Website-script.js
@@ -0,0 +1,48 @@
+gsap.from("#txt", {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: 350,
+})
+gsap.from("#m1", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 100,
+})
+gsap.from("#m2", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 50,
+})
+gsap.from("#m3", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 200,
+})
+gsap.from("#m4", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 150,
+})
+gsap.from("#tr", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 150,
+})
+gsap.from("#crs", {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: 100,
+})
+gsap.from("#house", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 50,
+})
